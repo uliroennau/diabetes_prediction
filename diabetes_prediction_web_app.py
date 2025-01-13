9c79ec84d6cd13b3baff75941557dcc5e101d763
Ulises Roennau <ulises.roennau@gmail.com>
2025-01-12 19:01:04 -0500

    Changing filepath of the model (try 1)

--- diabetes_prediction_web_app.py
+++ diabetes_prediction_web_app.py
@@ -3,7 +3,7 @@ import pickle
 import streamlit as st
 
 # loading the saved model
-loaded_model = pickle.load(open('/Users/uli/Documents/Projects/diabetes_prediction/trained_model.sav', 'rb'))
+loaded_model = pickle.load(open('trained_model.sav', 'rb'))
 
 # creating a function for prediction
 
